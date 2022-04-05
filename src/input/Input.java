a98210876b4301e793230c8e70e9d523fa579395
Jo2We <86071337+Jo2We@users.noreply.github.com>
2022-04-05 09:49:20 +0200

    enabled reading of LST files using encoding windows-1252

--- src/input/Input.java
+++ src/input/Input.java
@@ -1,20 +1,24 @@
 package input;
 
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Input {
     public void read() {
+        String encoding = "windows-1252";
         try {
-            File source = new File("res/test.txt");
-            Scanner myReader = new Scanner(source);
+            File source =  new File("res/TPicSim1.LST");
+            Scanner myReader = new Scanner(source, Charset.forName(encoding));
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
                 System.out.println(data);
             }
             myReader.close();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
