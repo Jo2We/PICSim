9a20ce4984e4ee4920f75e4f5d4fecc5d01ea0d5
FloBab73 <f.babel2000@gmail.com>
2022-04-05 09:12:21 +0200

    added .lst files

--- src/Input.java
+++ src/Input.java
@@ -5,7 +5,7 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class Input {
     void read() {
         try {
-            File source = new File("Diary.txt");
+            File source = new File("res/test.txt");
             Scanner myReader = new Scanner(source);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
