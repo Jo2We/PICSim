07ef255a5cc341ab1848fa008daf84cb26a2d119
Jo2We <86071337+Jo2We@users.noreply.github.com>
2022-04-05 10:02:53 +0200

    Read file and filters to relevant lines with code. Adds code to ArrayList

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,13 +2,18 @@ package controller;
 
 import input.Input;
 
+import java.util.ArrayList;
+
 public class Controller {
+
+    private ArrayList<String> lines = new ArrayList<String>();
 
     public Controller() {}
 
     public void runPICSimulator() {
         Input I = new Input();
-        I.read();
+        this.lines = I.read(this.lines);
+        this.lines.forEach((key) -> System.out.println(key));
     }
 
 }
--- src/input/Input.java
+++ src/input/Input.java
@@ -5,23 +5,28 @@ import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner; // Import the Scanner class to read text files
 
 public class Input {
-    public void read() {
+    public ArrayList<String> read(ArrayList<String> lines) {
         String encoding = "windows-1252";
         try {
             File source =  new File("res/TPicSim1.LST");
             Scanner myReader = new Scanner(source, Charset.forName(encoding));
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                char character = data.charAt(0);
+                if (character != ' ') {
+                    lines.add(data);
+                }
             }
             myReader.close();
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return lines;
     }
 }
 
