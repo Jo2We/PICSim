b489d2b8281ecae094b83f0909c389fdb2bf6736
FloBab73 <f.babel2000@gmail.com>
2022-04-06 08:46:13 +0200

    added addlw and sublw

--- src/PIC/Commands.java
+++ src/PIC/Commands.java
@@ -17,4 +17,14 @@ public class  Commands {
         int k = opcode - 12288; //-11 0000 0000 0000
         m.setW((char) k);
     }
+
+    void addlw(int opcode){
+        int k = opcode - 15872; //-11 1110 0000 0000
+        m.setW((char) (k+m.getW()));
+    }
+
+    void sublw(int opcode){
+        int k = opcode - 15360; //-11 1100 0000 0000
+        m.setW((char) (k-m.getW()));
+    }
 }
