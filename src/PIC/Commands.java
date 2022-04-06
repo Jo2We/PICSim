ce9215cd0f10cbb2c474d01c6a4afc4242ed4bda
FloBab73 <f.babel2000@gmail.com>
2022-04-06 14:05:03 +0200

    finished controller

--- src/PIC/Commands.java
+++ src/PIC/Commands.java
@@ -14,116 +14,182 @@ public class Commands {
     }
 
 
-    public void addwf(int dec) /*00 0111 -0000 0000 -|-1111 1111-*/ {
-        System.out.println("called addwf with " + dec);
+    public void addwf(int opcode) /*00 0111 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called addwf with " + opcode);
 
     }
 
-    public void andwf() /*00 0101 -0000 0000 -|-1111 1111-*/ {
+    public void andwf(int opcode) /*00 0101 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called andwf with " + opcode);
+
     }
 
-    public void clrf() /*00 0001 1 -000 0000 -|-111 1111-*/ {
+    public void clrf(int opcode) /*00 0001 1 -000 0000 -|-111 1111-*/ {
+        System.out.println("called clrf with " + opcode);
+
     }
 
-    public void clrw() /*00 0001 0 -xxx xxxx-*/ {
+    public void clrw(int opcode) /*00 0001 0 -xxx xxxx-*/ {
+        System.out.println("called clrw with " + opcode);
+
     }
 
-    public void comf() /*00 1001 0 -000 0000 -|-111 1111-*/ {
+    public void comf(int opcode) /*00 1001 0 -000 0000 -|-111 1111-*/ {
+        System.out.println("called comf with " + opcode);
+
     }
 
-    public void decf() /*00 0011 -0000 0000 -|-1111 1111-*/ {
+    public void decf(int opcode) /*00 0011 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called decf with " + opcode);
+
     }
 
-    public void decfsz() /*00 1011 -0000 0000 -|-1111 1111-*/ {
+    public void decfsz(int opcode) /*00 1011 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called decfsz with " + opcode);
+
     }
 
-    public void incf() /*00 1010 -0000 0000 -|-1111 1111-*/ {
+    public void incf(int opcode) /*00 1010 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called encf with " + opcode);
+
     }
 
-    public void incfsz() /*00 1111 -0000 0000 -|-1111 1111-*/ {
+    public void incfsz(int opcode) /*00 1111 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called incfsz with " + opcode);
+
     }
 
-    public void iorwf() /*00 0100 -0000 0000 -|-1111 1111-*/ {
+    public void iorwf(int opcode) /*00 0100 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called iorwf with " + opcode);
+
     }
 
-    public void movf() /*00 1000 -0000 0000 -|-1111 1111-*/ {
+    public void movf(int opcode) /*00 1000 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called movf with " + opcode);
+
     }
 
-    public void movwf() /*00 0000 1 -000 0000 -|-111 1111-*/ {
+    public void movwf(int opcode) /*00 0000 1 -000 0000 -|-111 1111-*/ {
+        System.out.println("called movwf with " + opcode);
+
     }
 
     public void nop() /*00 0000 0xx0 0000*/ {
+        System.out.println("called nop");
+
     }
 
-    public void rlf() /*00 1101 -0000 0000 -|-1111 1111-*/ {
+    public void rlf(int opcode) /*00 1101 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called rlf with " + opcode);
+
     }
 
-    public void rrf() /*00 1100 -0000 0000 -|-1111 1111-*/ {
+    public void rrf(int opcode) /*00 1100 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called rrf with " + opcode);
+
     }
 
-    public void subwf() /*00 0010 -0000 0000 -|-1111 1111-*/ {
+    public void subwf(int opcode) /*00 0010 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called subwf with " + opcode);
+
     }
 
-    public void swapf() /*00 1110 -0000 0000 -|-1111 1111-*/ {
+    public void swapf(int opcode) /*00 1110 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called swapf with " + opcode);
+
     }
 
-    public void xorwf() /*00 0110 -0000 0000 -|-1111 1111-*/ {
+    public void xorwf(int opcode) /*00 0110 -0000 0000 -|-1111 1111-*/ {
+        System.out.println("called xorwf with " + opcode);
+
     }
 
-    public void bcf() /*01 00*/ {
+    public void bcf(int opcode) /*01 00*/ {
+        System.out.println("called bcf with " + opcode);
+
     }
 
-    public void bsf() /*01 01*/ {
+    public void bsf(int opcode) /*01 01*/ {
+        System.out.println("called bsf with " + opcode);
+
     }
 
-    public void btfsc() /*01 10*/ {
+    public void btfsc(int opcode) /*01 10*/ {
+        System.out.println("called btfsc with " + opcode);
+
     }
 
-    public void btfss() /*01 11*/ {
+    public void btfss(int opcode) /*01 11*/ {
+        System.out.println("called btfss with " + opcode);
+
     }
 
     public void addlw(int opcode) /*11 111x*/ {
+        System.out.println("called addlw with " + opcode);
+
         int k = opcode - 15872; //-11 1110 0000 0000
         m.setW((char) (k + m.getW()));
     }
 
-    public void andlw() /*11 1001*/ {
+    public void andlw(int opcode) /*11 1001*/ {
+        System.out.println("called andlw with " + opcode);
+
     }
 
-    public void call() /*10 0*/ {
+    public void call(int opcode) /*10 0*/ {
+        System.out.println("called call with " + opcode);
+
     }
 
     public void clrwdt() /*00 0000 0110 0100*/ {
+        System.out.println("called clrwdt");
+
     }
 
-    public void _goto() /*10 0*/ {
+    public void _goto(int opcode) /*10 0*/ {
+        System.out.println("called _goto with " + opcode);
+
     }
 
-    public void iorlw() /*11 1000*/ {
+    public void iorlw(int opcode) /*11 1000*/ {
+        System.out.println("called iorlw with " + opcode);
+
     }
 
     public void movlw(int opcode) /*11 00xx*/ {
+        System.out.println("called movlw with " + opcode);
         int k = opcode - 12288; //-11 0000 0000 0000
         m.setW((char) k);
     }
 
     public void retfie() /*00 0000 0000 1001*/ {
+        System.out.println("called retfie");
+
     }
 
-    public void retlw() /*11 01xx*/ {
+    public void retlw(int opcode) /*11 01xx*/ {
+        System.out.println("called retlw with " + opcode);
+
     }
 
     public void _return() /*00 0000 0000 1000*/ {
+        System.out.println("called _return");
+
     }
 
     public void sleep() /*00 0000 0110 0011*/ {
+        System.out.println("called sleep");
+
     }
 
     public void sublw(int opcode) /*11 110x*/ {
+        System.out.println("called sublw with " + opcode);
         int k = opcode - 15360; //-11 1100 0000 0000
         m.setW((char) (k - m.getW()));
     }
 
-    public void xorlw() /*11 1010*/ {
+    public void xorlw(int opcode) /*11 1010*/ {
+        System.out.println("called xorlw with " + opcode);
+
     }
 }
--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -24,7 +24,7 @@ public class Controller {
     public void runPICSimulator() {
         Input I = new Input();
         //this.lines = I.read(this.lines);
-        this.lines = I.readTxt(this.lines);
+        this.lines = I.read(this.lines);
         this.lines.forEach((key) -> System.out.println(key + ": " + getBinary(key)));
         this.lines.forEach((key) -> callCommands(getBinaryAsInt(key)));
     }
@@ -108,40 +108,40 @@ public class Controller {
                                         // 00 0000 0111 | 112 - 127
                                     }
                                     // 00 0000 1 | 128 - 255 | movwf
-                                    command.movwf();
+                                    command.movwf(dec);
                                     return;
                                 }
                                 // 00 0001 | 256 - 511
                                 if (dec < 384) { // 00 0001 0 | 256 - 383 | clrw
-                                    command.clrw();
+                                    command.clrw(dec);
                                     return;
                                 }
                                 // 00 0001 1 | 384 - 511 | clrf
-                                command.clrf();
+                                command.clrf(dec);
                                 return;
                             }
                             // 00 001
                             if (dec < 768) { // 00 0010 | 512 - 767 | subwf
-                                command.subwf();
+                                command.subwf(dec);
                                 return;
                             }
                             // 00 0011 | 768 - 1024 | decf
-                            command.decf();
+                            command.decf(dec);
                             return;
                         }
                         // 00 01 | 1024 - 2047
                         if (dec < 1536) { // 00 010 | 1024 - 1535
                             if (dec < 1280) { // 00 0100 | 1024 - 1279 | iorwf
-                                command.iorwf();
+                                command.iorwf(dec);
                                 return;
                             }
                             // 00 0101 | 1280 - 1535 | andwf
-                            command.andwf();
+                            command.andwf(dec);
                             return;
                         }
                         // 00 011 | 1536 - 2047
                         if (dec < 1792) { // 00 0110 | 1536 - 1791 | xorwf
-                            command.xorwf();
+                            command.xorwf(dec);
                             return;
                         }
                         // 00 0111 | 1792 - 2047 | addwf
@@ -152,99 +152,104 @@ public class Controller {
                     if (dec < 3072) { // 00 10 | 2048 - 3071
                         if (dec < 2560) { // 00 100 | 2048 - 2559
                             if (dec < 2304) { // 00 1000 | 2048 - 2303 | movf
-                                command.movf();
+                                command.movf(dec);
                                 return;
                             }
                             // 00 1001 | 2304 - 2059 | comf
-                            command.comf();
+                            command.comf(dec);
                             return;
                         }
                         // 00 101 | 2560 - 3071
                         if (dec < 2816) { // 00 1010 | 2560 - 2815 | incf
-                            command.incf();
+                            command.incf(dec);
                             return;
                         }
                         // 00 1011 | 2816 - 3071 | decfsz
-                        command.decfsz();
+                        command.decfsz(dec);
                         return;
                     }
                     // 00 11 | 3072 - 4095
                     if (dec < 3584) { // 00 110 | 3072 - 3583
                         if (dec < 3328) { // 00 1100 | 3072 - 3327 | rrf
-                            command.rrf();
+                            command.rrf(dec);
                             return;
                         }
                         // 00 1101 | 3328 - 3583 | rlf
-                        command.rlf();
+                        command.rlf(dec);
                         return;
                     }
                     // 00 111 | 3584 - 4095
                     if (dec < 3840) { // 00 1110 | 3584 - 3839 | swapf
-                        command.swapf();
+                        command.swapf(dec);
                         return;
                     }
                     // 00 1111 | 3840 - 4095 | incfsz
-                    command.incfsz();
+                    command.incfsz(dec);
                     return;
                 }
                 // 01 | 4096 - 8191
                 if (dec < 6144) { // 01 0 4096 - 6143
                     if (dec < 5120) { // 01 00 | 4096 - 5119 | bcf
-                        command.bcf();
+                        command.bcf(dec);
                         return;
                     }
                     // 01 01 | 5120 - 6143 | bsf
-                    command.bsf();
+                    command.bsf(dec);
                     return;
                 }
                 // 01 1 | 6144 - 8191
                 if (dec < 7168) { // 01 10 | 6144 - 7167 | btfsc
-                    command.btfsc();
+                    command.btfsc(dec);
                     return;
                 }
                 // 01 11 | 7168 - 8191 | btfss
-                command.btfss();
+                command.btfss(dec);
                 return;
             }
             // --------------------------------------------------------------------------------
             // 1
             if (dec < 12288) { // 10
-                if (dec < 10240) { // 10 0
-                    if (dec < 9216) { // 10 00
-                        if (dec < 8704) { // 10 000
-                            if (dec < 8448) { // 10 0000
-                                if (dec < 8320) { // 10 0000 0
-                                    if (dec < 8256) { // 10 0000 00
-                                        if (dec < 8224) { // 10 0000 000
-                                            if (dec < 8208) { // 10 0000 0000
-                                                if (dec < 8200) { // 10 0000 0000 0
-                                                    if (dec < 8196) { // 10 0000 0000 00
-                                                        if (dec < 8194) { // 10 0000 0000 000
-
-                                                        }
-                                                        // 11 1111 1111 111
-                                                    }
-                                                    // 11 1111 1111 11
-                                                }
-                                                // 11 1111 1111 1
-                                            }
-                                            // 11 1111 1111
-                                        }
-                                        // 11 1111 111
-                                    }
-                                    // 11 1111 11
-                                }
-                                // 11 1111 1
-                            }
-                            // 11 1111
-                        }
-                        // 11 111
-                    }
-                    // 11 11
+                if (dec < 10240) { // 10 0 | call
+                    command.call(dec);
+                    return;
                 }
-                // 11 1
+                //10 1 | goto
+                command._goto(dec);
+                return;
             }
-            // 11
+            //11
+            if (dec < 14336) { //110
+                if (dec < 13312) { //1100 | movlw
+                    command.movlw(dec);
+                    return;
+                }
+                //1101 | retlw
+                command.retlw(dec);
+                return;
+            }
+            //111
+            if (dec < 15360) { //1110
+                if (dec < 14848) { //11100
+                    if (dec < 14592) { //111000 | iorlw
+                        command.iorlw(dec);
+                        return;
+                    }
+                    //111001 | andlw
+                    command.andlw(dec);
+                    return;
+                }
+                //11101(0) | xorlw
+                command.xorlw(dec);
+                return;
+            }
+            //1111
+            if (dec < 15872) { //11110 | sublw
+                command.sublw(dec);
+                return;
+            }
+            //11111 | addlw
+            command.addlw(dec);
+            return;
         }
         System.out.println("error");
     }
