5251eeb39704177cb9e207b8d56d4db66d6f75b2
Jo2We <86071337+Jo2We@users.noreply.github.com>
2022-04-07 11:33:09 +0200

    rebase development onto main

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,6 @@
 package controller;
 
+import GUI.MainFrame;
 import PIC.Commands;
 import input.Input;
 import PIC.Memory;
@@ -30,6 +31,7 @@ public class Controller {
      * method to run the PICSumulator and manages all steps
      */
     public void runPICSimulator() {
+        MainFrame mainFrame = new MainFrame();
         Input I = new Input();
         this.linesStr = I.read(this.linesStr);
         //this.lines.forEach((key) -> System.out.println(key + ": " + getBinary(key)));
