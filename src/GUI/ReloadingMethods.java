4c2839a16606de780f650dcc467f1c04b0fccd16
Jo2We <86071337+Jo2We@users.noreply.github.com>
2022-04-08 19:14:14 +0200

    finished memory maipulation of main memory, by using a additional JFrame, added RA and RB, with TRISA and TRISB, values can be toggled by click on it, RA, RB, TRIS for both are saved in Memory class

---
+++ src/GUI/ReloadingMethods.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import controller.Controller;
+
+public class ReloadingMethods extends MainFrame {
+
+    public ReloadingMethods(Controller controller) {
+        super(controller);
+    }
+}
